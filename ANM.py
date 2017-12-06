12c351c944a1a0476a3cc5df9481f1dd7e7dabba
Michael <mickeyjhg@gmail.com>
2017-12-06 17:11:01 +0200

    Python scripts to call C++ scripts

---
+++ ANM.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+# Simple python script to call getEigenVectors executable
+
+import os
+import sys
+import argparse
+
+# parse cmd arguments
+parser = argparse.ArgumentParser()
+
+# custom arguments
+parser.add_argument("--pdb", help="PDB input file")
+parser.add_argument("--cutoff", help="Cuttoff radius in Angstroms. Default: 15", default="15")
+parser.add_argument("--atomType", help="Select 'CA' or 'CB' atoms")
+parser.add_argument("--outdir", help="Directory to generate output to", default="output")
+
+args = parser.parse_args()
+
+# Check if required directories exist
+if not os.path.isdir(args.outdir):
+    os.makedirs(args.outdir)
+
+# Check if args supplied by user
+if len(sys.argv) > 1:
+
+    # Check if getEigenVectors exists
+    if not os.path.isfile("ANM"):
+        print ("Error: 'ANM' executable not found.")
+        print ("Please ensure it exists in the same directory as this script.")
+        sys.exit()
+
+    # Run getEigenVectors executable
+    os.system("./ANM --pdb " + args.pdb + " --cutoff " + args.cutoff + " --atomType " + args.atomType + " --outdir " + args.outdir)
+
+else:
+    print ('No arguments provided. Use -h to view help')
---
+++ getEigenVectors.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+# Simple python script to call getEigenVectors executable
+
+import os
+import sys
+import argparse
+
+# parse cmd arguments
+parser = argparse.ArgumentParser()
+
+# custom arguments
+parser.add_argument("--vt", help="VT matrix file")
+parser.add_argument("--mode", help="Specify the index of the mode you wish to target")
+parser.add_argument("--direction", help="Direction of overlap correction (Default -1)", default="-1")
+parser.add_argument("--outdir", help="Directory to generate output to", default="output")
+
+args = parser.parse_args()
+
+# Check if required directories exist
+if not os.path.isdir(args.outdir):
+    os.makedirs(args.outdir)
+
+# Check if args supplied by user
+if len(sys.argv) > 1:
+
+    # Check if getEigenVectors exists
+    if not os.path.isfile("getEigenVectors"):
+        print ("Error: 'getEigenVectors' executable not found.")
+        print ("Please ensure it exists in the same directory as this script.")
+        sys.exit()
+
+    # Run getEigenVectors executable
+    os.system("./getEigenVectors --vt " + args.vt + " --mode " + args.mode + " --direction " + args.direction + " --outdir " + args.outdir)
+
+else:
+    print ('No arguments provided. Use -h to view help')
