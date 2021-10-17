d52eea14a9ba4d53558f9ccdeecce71a7e5962ee
oliserand <oliserand@gmail.com>
2021-10-17 19:35:54 +0200

    rearranging

---
+++ src/welcome_msg.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+#filename: welcome_msg.py
+
+
+##===============================================================================
+##								 Welcome message
+##===============================================================================
+
+def main():
+
+	return;
+
+def welcome_msg(title):
+	'Print Welcome message'
+	tool_name='MODE-TASK'
+	print '\n\n'
+	print '\t|=======================================================|'
+	print '\t|\t\t\t\t\t\t\t|'
+	print '\t|\t :-) >>------->',tool_name,'<-------<< (-:\t|'
+	print '\t|\t\t\t\t\t\t\t|'
+	print '\t|\t    :-) ----------',title,'---------- (-:\t\t|'
+	print '\t|\t\t\t\t\t\t\t|'
+	print '\t|\t\t\t\t\t\t\t|'
+	print '\t|\tThis programe performs the', title,' \t\t| \n\t|\ton a MD trajectory\t\t\t\t|'
+	print '\t|\t\t\t\t\t\t\t|\n', '\t|\tAuthors:  Bilal Nizami\t\t\t\t|\n','\t|\tResearch Unit in Bioinformatics (RUBi)\t\t|\n', '\t|\tRhodes University, 2017\t\t\t\t|'
+	print '\t|\tDistributed under GNU GPL 3.0\t\t\t|'
+	print '\t|\t\t\t\t\t\t\t|'
+	print '\t|\thttps://github.com/michaelglenister/NMA-TASK\t|'
+	print '\t|\t\t\t\t\t\t\t|'
+	print '\t|=======================================================|'
+	print '\n'
+	return;
+	
+if __name__=="__main__":
+	main()
