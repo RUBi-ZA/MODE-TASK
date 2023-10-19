fff11c4c855fceace9876ba5b81ea7c38da8043a
oliserand <olisreand@gmail.com>
2023-10-19 19:01:15 +0200

    Fixed ANM test

--- tests/test_anm.sh
+++ tests/test_anm.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
 
+file="input_pdb"
+while IFS= read line
+do
+    prot=${line}
+done <${file}
+
 cd ../src
+
+./coarseGrain.py --pdb ../tests/${prot}.pdb --cg 4 --atomType CB --outdir ../output/
 
 g++ -I cpp/src/ ANM.cpp -o ANM
 
-../src/ANM --pdb ../outdir/ComplexCG4.pdb --cutoff 24 --atomType CB
+../src/ANM --pdb ../output/ComplexCG4.pdb --cutoff 24 --atomType CB
+
+
+
+
+
+
+
